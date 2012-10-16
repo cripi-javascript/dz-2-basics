1501b45364dd55ca08969ec1eeb431f79352dd08
aki <aki@ein.(none)>
2012-10-16 20:06:02 +0600

    + маленькая проверка типа для одной из переменных

--- event.js
+++ event.js
@@ -1,11 +1,11 @@
 /**
  * Возвращает объект Event
  *
- * @param {Number|Date} start           	  Начало события
- * @param {Number|Date} end             	  Конец события
- * @param {String}      [name="Событие"]	  Имя события
+ * @param {Number|Date} start                     Начало события
+ * @param {Number|Date} end                       Конец события
+ * @param {String}      [name="Событие"]          Имя события
  * @param {String}	[description="Описание"]  Описание события
- * @param {Boolean}	alarm			  Нужно ли напоминание
+ * @param {Boolean}	[alarm=false]	          Нужно ли напоминание
  *
  * @example
  *    Event(new Date('2011-10-10T14:48:00'),
@@ -15,11 +15,16 @@
  * @return {Object}
  */
 function Event(start, end, name, description, alarm) {
+    if (typeof(alarm) === "boolean") {
+        var new_alarm = alarm;
+    } else {
+        var new_alarm = false;
+    }
     return {
         "start": +start,
         "end": +end,
         "name": name || "Событие",
         "description": description || "Описание",
-        "alarm": alarm || false
+        "alarm": new_alarm
     };
 }
