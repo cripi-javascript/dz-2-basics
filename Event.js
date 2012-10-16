69a2fb5bdfd8d7897eaf848ff98439d80250087b
Artem <WesperMc@mail.ru>
2012-10-16 23:32:41 +0600

    Create my event function

---
+++ Event.js
@@ -0,0 +1,28 @@
+/*
+ * Возвращает объект Event
+ *
+ * @param {Number|Date} start             Начало события
+ * @param {Number|Date} end               Конец события
+ * @param {String}      [name="Событие"]  Имя события
+ * @param {String}      [place="Место"]      Место события
+ * @param {Number} GPS                    Координаты события
+ *
+ * @example
+ *    Event(new Date('2011-10-10T14:48:00'),
+ *          new Date('2011-10-10T15:48:00'),
+ *          "Совещание",
+ *          "Хаят",
+ *          "624:342" )
+ *
+ * @return {Object}
+ */
+
+function Event(start, end, name, place, GPS) {
+    return {
+        "start": +start,
+        "end": +end,
+        "name": name || "Событие",
+        "place": place ||  "Место",
+        "GPS": GPS || "624:342
+    };
+}
