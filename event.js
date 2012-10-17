a455f0965b51483932edb1d74297a6c9fcfc25c1
aki <aki@ein.(none)>
2012-10-17 17:28:37 +0600

    + rating

--- event.js
+++ event.js
@@ -5,6 +5,7 @@
  * @param {Number|Date} end                       Конец события
  * @param {String}      [name="Событие"]          Имя события
  * @param {String}	[description="Описание"]  Описание события
+ * @param {Number}      [number=1]                Рейтинг события  
  * @param {Boolean}	[alarm=false]	          Нужно ли напоминание
  *
  * @example
@@ -14,17 +15,25 @@
  *
  * @return {Object}
  */
-function Event(start, end, name, description, alarm) {
+function Event(start, end, name, description, rating, alarm) {
     if (typeof(alarm) === "boolean") {
         var new_alarm = alarm;
     } else {
         var new_alarm = false;
+    }
+    if ((typeof(rating) === "number")) {
+        if (rating > 5 || rating < 1) {
+            var new_rating = 1;
+        } else {
+            var new_rating = rating;
+        }
     }
     return {
         "start": +start,
         "end": +end,
         "name": name || "Событие",
         "description": description || "Описание",
+        "rating": new_rating,
         "alarm": new_alarm
     };
 }
