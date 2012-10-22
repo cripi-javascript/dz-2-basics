edb8896d5e603d340c3eaba000194f6291d3123f
Artem <WesperMc@mail.ru>
2012-10-22 19:11:33 +0600

    Добавлена проверка на правильность GPS
    И описание события

--- Event.js
+++ Event.js
@@ -1,28 +1,40 @@
-﻿/*
+﻿/*jslint plusplus: true, vars: true, browser: true, devel: true */
+/*
  * Возвращает объект Event
  *
  * @param {Number|Date} start             Начало события
  * @param {Number|Date} end               Конец события
  * @param {String}      [name="Имя"]      Название события
  * @param {String}      [place="Место"]   Место события
- * @param {Number} GPS                    Координаты события
+ * @param {String}      [definition="Описание"]   Место события
+ * @param {Number} GPS [lon = "Долгота", lat = "Широта"]  GPS координаты места 
  *
  * @example
  *    Event(new Date('2011-10-10T14:48:00'),
  *          new Date('2011-10-10T15:48:00'),
- *          "Совещание",
+ *          "Совещание",							
  *          "Хаят",
- *          "624:342" )
+ *			"Повышение в должности"
+ *          "34.342, 60.354" )
  *
  * @return {Object}
  */
 
-function Event(start, end, name, place, GPS) {
+function Event(start, end, name, place, definition, GPS) {
+    "use strict";
+    if (typeof GPS === "number") {
+	    if (GPS[0] > Math.abs(90) || GPS[1] > Math.abs(180)) {
+            var new_GPS = [0, 0];
+        } else {
+            var new_GPS = GPS;
+        }
+    }
     return {
         "start": +start,
         "end": +end,
         "name": name || "Имя",
         "place": place ||  "Место",
-        "GPS": GPS || "624:342"
+	    "definition": definition || "Описание",
+        "GPS": GPS || "Долгота, Широта"
     };
 }
