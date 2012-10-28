da04e65d0341f79c885273253882207d3fb16289
Alexey Sotov <grave@isnet.ru>
2012-10-29 01:26:50 +0600

    Переписаны данные примеров, дописана функция addEvent

--- event.js
+++ event.js
@@ -1,26 +1,34 @@
 ﻿var requiredParams = {
-	start: new Date('27-10-2012T09:00:00'),
-	end: new Date('28-10-2012T22:00:00'),
-	name: 'Поездка на озеро Тургояк',
-	isNotify: 'true',
-	notify: new Date('26-10-2012T20:00:00)
-	};
+    start: new Date('10-26-2012 02:12'),
+    end: new Date('10-27-2012'),
+    name: 'Поездка на озеро Тургояк',
+    isNotify: 'true',
+    notify: new Date('10-26-2012')
+    };
 var optionalParams = {
-	description: 'Отличная возможность отдохнуть и насладиться природой',
-	place: 'Озеро Тургояк',
-	importance: 'h',
-	url: 'http://www.turgoyak.com/'
-	};
+    description: 'Отличная возможность отдохнуть и насладиться природой',
+    place: 'Озеро Тургояк',
+    importance: 'h',
+    url: 'http://www.turgoyak.com/'
+    };
 function addEvent(requiredParams, optionalParams) {
-	"use strict";
-	if (typeof requiredParams.start === 'Date') {
-		if (typeof requiredParams.end === 'Date') {
-			return {
-				start: requiredParams.start,
-				end: requiredParams.end,
-				name: requiredParams.name || "Событие",
-				isNotify: isNotify || "false"
-				
-		}
-	}
-	
+"use strict";
+if (requiredParams.start > requiredParams.end) {
+    throw new Error("Время начала мероприятия не может быть позже времени окончания!");
+    }
+var imp = optionalParams.importance;
+if (!(imp == 'h' || imp == 'l' || imp == 'm')) {
+    imp = 'm';
+}
+return {
+    start: new Date(+requiredParams.start),
+    end: new Date(+requiredParams.end),
+    name: requiredParams.name || "Событие",
+    isNotify: !!requiredParams.isNotify || false,
+    notify: +requiredParams.notify,
+    description: optionalParams.description || "",
+    place: optionalParams.place || "",
+    importance: imp,
+    url: optionalParams.url
+    };   
+}
