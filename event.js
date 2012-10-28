eb90c0d2a11a3d87acf98e97c1dbeb2530656cdc
Alexey Sotov <grave@isnet.ru>
2012-10-29 01:51:17 +0600

    Задокументировано

--- event.js
+++ event.js
@@ -1,17 +1,28 @@
-﻿var requiredParams = {
-    start: new Date('10-26-2012 02:12'),
-    end: new Date('10-27-2012'),
-    name: 'Поездка на озеро Тургояк',
-    isNotify: 'true',
-    notify: new Date('10-26-2012')
-    };
-var optionalParams = {
-    description: 'Отличная возможность отдохнуть и насладиться природой',
-    place: 'Озеро Тургояк',
-    importance: 'h',
-    url: 'http://www.turgoyak.com/'
-    };
-function addEvent(requiredParams, optionalParams) {
+﻿/**
+ * Возвращает объект Event
+ *
+ * @param {Object} requiredParams    Обязательные параметры
+ * @param {Object} optionalParams    Дополнительные параметры
+ * 
+ * @example
+ * 		var required = {
+ *			start: new Date('10-26-2012 02:12'),
+ *			end: new Date('10-27-2012'),
+ *			name: 'Поездка на озеро Тургояк',
+ *			isNotify: 'true',
+ *			notify: new Date('10-26-2012')
+ *		};
+ * 		var optional = {
+ *			description: 'Отличная возможность отдохнуть и насладиться природой',
+ *			place: 'Озеро Тургояк',
+ *			importance: 'h',
+ *			url: 'http://www.turgoyak.com/'
+ *		};
+ *    	getEvent(required, optional);
+ *
+ * @return {Object}
+ */
+function getEvent(required, optional) {
 "use strict";
 if (requiredParams.start > requiredParams.end) {
     throw new Error("Время начала мероприятия не может быть позже времени окончания!");
