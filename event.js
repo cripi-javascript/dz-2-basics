a63a40195947b10416dd52d3716d6312263a84f2
Borisov Dmitry <ya.whiteguy@yandex.ru>
2012-10-30 23:30:15 +0600

    Event object with input data validation

---
+++ event.js
@@ -0,0 +1,119 @@
+/*jslint devel: true */
+function checkStartDate(date) {
+	'use strict';
+	if (date === null) {
+		date = new Date();
+	} else if (!(date instanceof Date && isFinite(date))) {
+		throw new Error("Start date is invalid, check syntax");
+	}
+	return date;
+}
+
+function checkEndDate(endDate, startDate) {
+	'use strict';
+	var date;
+	if (endDate === null) {
+		date = startDate;
+		date.setHours(startDate.getHours() + 1);
+	} else if (endDate instanceof Date && isFinite(endDate)) {
+		date = endDate;
+	} else {
+		throw new Error("End date is invalid, check syntax");
+	}
+	return date;
+}
+
+var REPEAT = {
+	NEVER: {title: "None", value: "+ 0.0.0 0:0"},
+	DAY: {title: "Every Day", value: "+ 1.0.0 0:0"},
+	WEEK: {title: "Every Week", value: "+ 7.0.0 0:0"},
+	TWOWEEK: {title: "Every 2 weeks", value: "+ 14.0.0 0:0"},
+	MONTH: {title: "Every month", value: "+ 0.1.0 0:0"},
+	YEAR: {title: "Every year", value: "+ 0.0.1 0:0"}
+};
+
+var ALERT = {
+	NONE: {title: "None", value: "+ 0.0.0 0:0"},
+	B5MIN: {title: "5 minutes before", value: "- 0.0.0 0:5"},
+	B15MIN: {title: "15 minutes before", value: "- 0.0.0 0:15"},
+	B30MIN: {title: "30 minutes before", value: "- 0.0.0 0:30"},
+	B1HOUR: {title: "1 hour before", value: "- 0.0.0 1:0"},
+	B1DAY: {title: "1 day before", value: "- 0.0.0 24:0"}
+};
+
+function checkAddTime(addTime) {
+	'use strict';
+	var re, splitted;
+	re = "([+-]) (\\d?\\d.\\d?\\d.\\d?\\d) (\\d?\\d:\\d?\\d)";
+	splitted = addTime.match(re);
+	return splitted.length === 4;
+}
+
+function checkRepeat(repeat) {
+	'use strict';
+	if (repeat === null) {
+		repeat = REPEAT.NEVER;
+	} else if (!(repeat.title && repeat.value)) {
+		throw new Error("Unknown type of 'repeat' variable");
+	} else if (!checkAddTime(repeat.value)) {
+		throw new Error("Add time in 'repeat' variable must have format '+ dd.MM.YY hh:mm'");
+	}
+	return repeat;
+}
+
+function checkAlert(alert) {
+	'use strict';
+	if (alert === null) {
+		alert = ALERT.NONE;
+	} else if (!(alert.title && alert.value)) {
+		throw new Error("Unknown type of 'alert' variable");
+	} else if (!checkAddTime(alert.value)) {
+		throw new Error("Add time in 'alert' variable must have format '+ dd.MM.YY hh:mm'");
+	}
+	return alert;
+}
+
+/**
+ * Возвращает объект Event
+ *
+ * @param {String}      [title="New Event"]     Имя события
+ * @param {String}      [location]              Место события
+ * @param {Number|Date} [starts="new Date()"]   Начало события
+ * @param {Number|Date} [ends="starts + 1"]     Конец события
+ * @param {Object}      [repeat="REPEAT.NEVER"] Периодичность события
+ * @param {Object}      [alert="ALERT.NONE"]    Предупреждение
+ * @param {String}      [notes]                 Заметки
+ *
+ * @example
+ *      Event("Лекция JavaScript",
+ *          "УРГУ",
+ *          new Date('2011-10-10T14:48:00'),
+ *          new Date('2011-10-10T15:48:00'),
+ *          REPEAT.WEEK,
+ *          ALERT.B30MIN,
+ *          "Вспомнить, что проходили на прошлом занятии")
+ *
+ * @return {Object}
+ */
+function Event(title, location, starts, ends, repeat, alert, notes) {
+	'use strict';
+	var startDate, endDate;
+	try {
+		startDate = checkStartDate(starts);
+		endDate = checkEndDate(ends, starts);
+		repeat = checkRepeat(repeat);
+		alert = checkAlert(alert);
+	} catch (e) {
+		console.log(e.message);
+		return;
+	}
+	return {
+		"title": title || "New Event",
+		"location": location,
+		"startDate": startDate,
+		"endDate": endDate,
+		"repeat": repeat,
+		"alert": alert,
+		"notes": notes
+	};
+}
